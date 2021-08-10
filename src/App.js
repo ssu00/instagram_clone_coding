74a4db451752773cd1abde925918ee1b803478f9
ssu00 <rachel4535@naver.com>
2021-08-10 22:56:00 +0900

    main

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Begin, Second, RadioButton } from "./components/SignUp";
+const App = () => {
+  return <RadioButton props={"전화"}></RadioButton>;
+};
+
+export default App;
