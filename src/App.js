5ef147b60a836bf22f1f21208748c39104d71947
ssu00 <rachel4535@naver.com>
2021-08-11 11:58:52 +0900

    메인

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Begin, Second, RadioButton } from "./components/SignUp";
+import { Begin, Second, RadioButton,Third,Fourth } from "./components/SignUp";
+
 const App = () => {
-  return <RadioButton props={"전화"}></RadioButton>;
+  return <Fourth></Fourth>
 };
 
 export default App;
