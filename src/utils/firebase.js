a93c1a034e6032f2853282980be02ad79c01e39a
ssu00 <rachel4535@naver.com>
2021-08-14 22:08:16 +0900

    firebase setting

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,7 +1,11 @@
-import * as firebase from "firebase";
+import firebase from "firebase";
 import config from "../../firebase.json";
 const app = firebase.initializeApp(config);
-export const signup = async ({ email, password }) => {
-  const { user } = await Auth.createUserWithEmailAndPassword(email, password);
+const Auth=app.auth();
+export const signup = async ({ email,password }) => {
+  const { user } = await Auth.createUserWithEmailAndPassword(email, password).then(()=>{
+    let user=auth.currentUser;
+    user.sendEmailVerification().then(function(){console.log("메일 전송");}).catch("Email not Sent!");
+  }).catch((error)=>console.log(error));
   return user;
 };
