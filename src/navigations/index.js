4d47e0aa41d88c3f4028c5d9a0c70c6cbeda9a55
zoowb <zoowb0829@gmail.com>
2021-08-15 20:41:53 +0900

    로그인 화면

---
+++ src/navigations/index.js
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AuthStack from './AuthStack';
+
+const Navigation = () => {
+    return (
+        <NavigationContainer>
+            <AuthStack />
+        </NavigationContainer>
+    );
+};
+
+export default Navigation;
---
+++ src/utils/images.js
@@ -0,0 +1,6 @@
+const prefix = 
+    'https://firebasestorage.googleapis.com/v0/b/react-native-instagram-d1f18.appspot.com/o';
+
+export const images = {
+    logo: `${prefix}/logo.png?alt=media`,
+};
