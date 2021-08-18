982ce3d4cbf3ad3b650b36cc1fe108ce5910dd55
ssu00 <rachel4535@naver.com>
2021-08-18 20:48:34 +0900

    🩹 arrange codes(signup's buttons,boxes)

--- src/components/SignUp/btn_inputs/inputBox.js
+++ src/components/SignUp/btn_inputs/inputBox.js
@@ -1,19 +1,26 @@
 import React from "react";
 import styled from "styled-components/native";
+import color from "../palette";
 const StyledInput = styled.TextInput`
   width: 100%;
   height: 50px;
-  color: #999999;
+  color: ${color.grey_1};
   font-weight: 700;
-  background-color: #fafafa;
-  border: 0.5px solid #dbdbdb;
+  background-color: ${color.grey_2};
+  border: 0.5px solid  ${color.grey_3};
   border-radius: 6px;
   margin-bottom: 20px;
   padding-left: 15px;
 `;
 
 const InputBox = ({ placeholder,handleChange,category,encrypted,value }) => {
-  return <StyledInput placeholder={placeholder} onChangeText={handleChange(`${category}`)} secureTextEntry={encrypted} value={value}/>;
+  return (
+    <StyledInput
+      placeholder={placeholder}
+      onChangeText={handleChange(`${category}`)}
+      secureTextEntry={encrypted}
+      value={value}
+    />);
 };
 
 export default InputBox;
