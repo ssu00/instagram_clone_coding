9530bf463d009d156149494b8e934d18452fefd5
ssu00 <rachel4535@naver.com>
2021-08-18 21:18:18 +0900

    🚚 changed components' names

---
+++ src/navigations/Login.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from 'styled-components/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Login, Step01 } from '../screens';
+const Stack = createStackNavigator();
+const LoginStack = () => {
+  const theme = useContext(ThemeContext);
+  return (
+    <Stack.Navigator
+      initialRouteName="Login"
+      screenOptions={{
+        headerTitleAlign: 'center',
+        cardStyle: { backgroundColor: theme.background },
+        headerTintColor: theme.headerTintColor,
+      }}
+    >
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Signup"
+        component={Step01}
+        options={{ headerBackTitleVisible: false}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default LoginStack;
