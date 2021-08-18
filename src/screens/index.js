8a73713b4bee9fc17361d77cf60eb09c62d4d579
ssu00 <rachel4535@naver.com>
2021-08-18 21:13:34 +0900

    added index files

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,4 +1,4 @@
-import Login from './Login';
-import Signup from './Signup';
-
-export { Login, Signup };
+import { InitScreen } from "./Common";
+import { Login } from "./Login";
+import {Step01,Step02,Step03,Step04,Step05,Step06,Step07} from "./SignUp";
+export {InitScreen,Login,Step01,Step02,Step03,Step04,Step05,Step06,Step07};
