48c625c6215856a19392f5a05796c5ec2779e8d5
ssu00 <rachel4535@naver.com>
2021-08-18 21:12:40 +0900

    🎨 MainNavigator (For Entire Project)

--- src/navigations/Main.js
+++ src/navigations/Main.js
@@ -1,16 +1,32 @@
-// import React from "react";
-// import SignUp from '../components/SignUp/SignUpTotal';
-// import MyPage from '../components/MyPage/MyPage';
-// import { createStackNavigator, createAppContainer } from '@react-navigation/stack';
-// import { Begin } from '../components/SignUp';
-// const AppNavigator = {
-//     Begin,
-//     SignUp,
-//     MyPage
-// }
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { InitScreen } from "../screens";
+import { NavigationContainer } from "@react-navigation/native";
+import Registration from "../components/SignUp/Registration";
+import LoginStack from "./Login";
+const Stack = createStackNavigator();
+const MainNavigator = () => {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator screenOptions={{ cardStyle: { backgroundColor: "#ffffff" } }}>
+            <Stack.Screen name="Init" options={{ headerShown: false }}>
+            {({ navigation }) => (
+            <InitScreen navigation={navigation} />
+            )}
+            </Stack.Screen>
+            <Stack.Screen name="Register" options={{ headerShown: false }}>
+                {({ navigation }) => (
+                <Registration navigation={navigation} />
+                )}
+            </Stack.Screen>
+            <Stack.Screen name="SignIn" options={{ headerShown: false }}>
+                {({ navigation }) => (
+                <LoginStack navigation={navigation} />
+                )}
+            </Stack.Screen>
+        </Stack.Navigator>
+    </NavigationContainer>
+    );
+};
 
-// const AppNav=createAppContainer(AppNavigator, {
-//     initialRouteName: 'Begin',
-//   });
-
-// export default AppNav;
+export default MainNavigator;
